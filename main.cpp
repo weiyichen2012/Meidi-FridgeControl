d8753276797c0c20076d64ce6c98c8d0bbf0d4c5
ethepherein <ethepherein@sjtu.edu.cn>
2021-09-26 13:04:56 +0800

    "implementA*"

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <set>
+#include <cstdlib>
 #include <tuple>
 using namespace std;
 
@@ -30,6 +33,18 @@ struct _unit{
         this->cargo = cargo;
     }
 
+    int getId(){
+        return id;
+    }
+
+    int getStage(){
+        return stage;
+    }
+
+    string getCargo(){
+        return cargo;
+    }
+
     //store a cargo into container
     void storeCargo(string cargo){
         if (this->stage != unitContainer){
@@ -40,8 +55,8 @@ struct _unit{
         this->cargo = cargo;
     }
 
-    //get a cargo from container
-    string getCargo(){
+    //remove a cargo from container
+    string removeCargo(){
         if (this->stage != unitFull){
             cout << "Error in unit's getCargo(), cannot get a unit that's not unitFull, its status: " << this->stage << endl;
             return "Error";
@@ -60,8 +75,8 @@ struct _unit{
         this->cargo = fromContainer->cargo;
     }
 
-    //get whole container from car
-    void getContainer(){
+    //remove whole container from car
+    void removeContainer(){
         if (this->stage != unitContainer && this->stage != unitFull){
             cout << "Error in unit's getContainer(), cannot get container from unit that has no container, its status: " << this->stage << endl;
             return;
@@ -98,21 +113,21 @@ struct _unit{
     }
 };
 
+// the global target
 int targetId[N][N][N];
 tuple<int, int, int> targetPos[N * N * N];
-void initTargetPos(){
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j)
-            for (int k = 0; k < N; ++k)
-                targetPos[targetId[i][j][k]] = make_tuple(i, j, k);
-}
+
+int deltaMoveX[4] = {-1, 1, 0, 0};
+int deltaMoveY[4] = {0, 0, -1, 1};
 
 struct _layer{
     int id, unitId[N][N], manhattan;
     _unit unit[N][N];
+    vector<string> moves;
 
     _layer(){manhattan = -1;}
     _layer(int id){
+        manhattan = -1;
         this->id = id;
         for (int i = 0; i < N; ++i)
             for (int j = 0; j < N; ++j){
@@ -124,7 +139,40 @@ struct _layer{
                 else
                     unit[i][j] = _unit(unitContainer, unitId[i][j]);
             }
+    }
+
+    void changeId(int* inputId){
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < N; ++j){
+                cout << i << " " << j << endl;
+                unitId[i][j] = inputId[i * N + j];
+                unit[i][j].id = inputId[i * N + j];
+                unit[i][j].stage = unitContainer;
+            }
+        unit[0][0].stage = unitNull;
+    }
+
+    void switchUnit(tuple<int, int> thing, tuple<int, int> null){
         manhattan = -1;
+        // moves.push_back("Move (" + to_string(get<0>(thing)) + ", " + to_string(get<1>(thing)) + ") -> (" + to_string(get<0>(null)) + ", " + to_string(get<1>(null)) + ")");
+        moves.push_back("Move " + to_string(unitId[get<0>(thing)][get<1>(thing)]));
+
+        _unit tmpUnit = unit[get<0>(thing)][get<1>(thing)];
+        unit[get<0>(thing)][get<1>(thing)] = unit[get<0>(null)][get<1>(null)];
+        unit[get<0>(null)][get<1>(null)] = tmpUnit;
+
+        int tmpId = unitId[get<0>(thing)][get<1>(thing)];
+        unitId[get<0>(thing)][get<1>(thing)] = unitId[get<0>(null)][get<1>(null)];
+        unitId[get<0>(null)][get<1>(null)] = tmpId;
+    }
+
+    string getHash(){
+        string rtnString = "";
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < N; ++j)
+                rtnString += to_string(unitId[i][j]);
+        // cout << rtnString << endl;
+        return rtnString;
     }
 
     int getManhattan(){
@@ -133,9 +181,30 @@ struct _layer{
         int sum = 0;
         for (int i = 0; i < N; ++i)
             for (int j = 0; j < N; ++j)
-                sum += abs(id - get<0>(targetPos[unit[i][j].id])) + abs(i - get<1>(targetPos[unit[i][j].id])) + abs(j - get<2>(targetPos[unit[i][j].id]));
+                sum += abs(id - get<0>(targetPos[unit[i][j].getId()])) + abs(i - get<1>(targetPos[unit[i][j].getId()])) + abs(j - get<2>(targetPos[unit[i][j].getId()]));
         manhattan = sum;
         return sum;
+    }
+
+    vector<_layer> neighbors(){
+        vector<_layer> rtnNeighbors;
+        vector<tuple<int, int>> nullPos;
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < N; ++j){
+                if (this->unit[i][j].getStage() == unitNull)
+                    nullPos.push_back(make_tuple<int, int>(move(i), move(j)));
+            }
+        for (vector<tuple<int, int>>::iterator it = nullPos.begin(); it != nullPos.end(); it++){
+            int posX = get<0>(*it), posY = get<1>(*it);
+            for (int i = 0; i < 4; ++i){
+                if (0 <= posX + deltaMoveX[i] && posX + deltaMoveX[i] < N && 0 <= posY + deltaMoveY[i] && posY + deltaMoveY[i] < N && this->unit[posX + deltaMoveX[i]][posY + deltaMoveY[i]].getStage() != unitNull){
+                    _layer tmpLayer = (*this);
+                    tmpLayer.switchUnit(make_tuple<int, int>(posX + deltaMoveX[i], posY + deltaMoveY[i]), make_tuple<int, int>(move(posX), move(posY)));
+                    rtnNeighbors.push_back(tmpLayer);
+                }
+            }
+        }
+        return rtnNeighbors;
     }
 
     void print(){
@@ -145,6 +214,20 @@ struct _layer{
                 if (j == N - 1)
                     cout << endl;
             }
+    }
+
+    void printMove(){
+        int n = 1;
+        for (vector<string>::iterator it = moves.begin(); it != moves.end(); ++it){
+            cout << n <<  ": " << (*it) << endl;
+            n++;
+        }
+    }
+};
+
+struct cmp{
+    bool operator()(_layer& l1, _layer& l2){
+        return l1.getManhattan() > l2.getManhattan();
     }
 };
 
@@ -164,21 +247,82 @@ struct _fridge{
     }
 }fridge;
 
+struct _solver{
+    priority_queue<_layer, vector<_layer>, cmp> solution;
+    set<string> alreadySolution;
+
+    _solver(){
+
+    }
+
+    tuple<int, int, int> *getTargetPos(){
+        return targetPos;
+    }
+
+    void initTargetPos(){
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < N; ++j)
+                for (int k = 0; k < N; ++k)
+                    targetPos[targetId[i][j][k]] = make_tuple(i, j, k);
+    }
+
+    void solve(_layer start){
+        initTargetPos();
+        while (!solution.empty()){
+            solution.pop();
+        }
+        solution.push(start);
+        alreadySolution.insert(start.getHash());
+        while (1){
+            // this->printSolver();
+            // cin.get();
+            _layer topLayer = solution.top();
+            if (topLayer.getManhattan() == 0)
+                break;
+            solution.pop();
+            cout << "Solving..." << endl;
+            vector<_layer> neighbors = topLayer.neighbors();
+            for (vector<_layer>::iterator it = neighbors.begin(); it != neighbors.end(); it++){
+                if (alreadySolution.find(it->getHash()) == alreadySolution.end()){
+                    solution.push(*it);
+                    alreadySolution.insert(it->getHash());
+                }
+            }
+
+        }
+        _layer ans = solution.top();
+        ans.printMove();
+    }
+
+
+    void printSolver(){
+        cout << "!" << endl;
+        priority_queue<_layer, vector<_layer>, cmp> cpSolution = solution;
+        cout << "?" << endl;
+        int n = 0;
+        cout << "Printing Solutions" << endl;
+        while (!cpSolution.empty()){
+            _layer tmpLayer = cpSolution.top();
+            cout << "Solution " << n << " with mahattan = " << tmpLayer.getManhattan() << endl;
+            n++;
+            tmpLayer.print();
+            cpSolution.pop();
+            cout << endl;
+        }
+    }
+}solver;
+
 int main(){
-    fridge.print();
+    // fridge.print();
     for (int i = 0; i < N; ++i)
         for (int j = 0; j < N; ++j)
             for (int k = 0; k < N; ++k)
                 targetId[i][j][k] = i * N * N + j * N + k;
-    targetId[0][0][0] = 0;
-    targetId[0][0][1] = 1;
-    targetId[0][0][2] = 2;
-    targetId[0][1][0] = 3;
-    targetId[0][1][1] = 4;
-    targetId[0][1][2] = 5;
-    targetId[0][2][0] = 6;
-    targetId[0][2][1] = 7;
-    targetId[0][2][2] = 8;
-    initTargetPos();
-    cout << fridge.layer[0].getManhattan() << endl;
+    int t[N * N];
+    t[0 * N + 0] = 0; t[0 * N + 1] = 6; t[0 * N + 2] = 8;
+    t[1 * N + 0] = 1; t[1 * N + 1] = 2; t[1 * N + 2] = 5;
+    t[2 * N + 0] = 7; t[2 * N + 1] = 4; t[2 * N + 2] = 3;
+    _layer testLayer(0);
+    testLayer.changeId(t);
+    solver.solve(testLayer);
 }
